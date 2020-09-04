3e67be0a88b8af927108526e37eac7f18f11e6ec
jd <duverseaujohnny21@gmail.com>
2020-09-04 19:31:22 -0400

    all test pass and indented

--- _tests_/galactic.test.js
+++ _tests_/galactic.test.js
@@ -1,6 +1,5 @@
 import Planets from './../src/galactic.js'
 // import { TestScheduler } from 'jest'
-
 describe('Planets', () => {
   
   let planets;
@@ -9,9 +8,8 @@ describe('Planets', () => {
   }) 
 
   test("It will show  you the user current age in Mercury", () => {
-   
-   planets.checkMercury()
-   expect(planets.age).toEqual(158)
+    planets.checkMercury()
+    expect(planets.age).toEqual(158)
   })
 
   test("It will show you the users current age in Venus", () => {
