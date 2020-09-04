f31155310f88b5237552cd5c82de5e2ea40fcceb
jd <duverseaujohnny21@gmail.com>
2020-09-04 16:13:45 -0400

    refactor with beforeEach() and pass

--- _tests_/galactic.test.js
+++ _tests_/galactic.test.js
@@ -2,10 +2,15 @@ import Planets from './../src/galactic.js'
 // import { TestScheduler } from 'jest'
 
 describe('Planets', () => {
+  
+  let planets;
+  beforeEach(() => {
+    planets = new Planets(38, 90)
+  }) 
 
   test("It will show  you the user current age in Mercury", () => {
-   const mercury = new Planets(38,90)
-   mercury.checkMercury()
-   expect(mercury.age).toEqual(70)
+   
+   planets.checkMercury()
+   expect(planets.age).toEqual(158)
   })
 })
