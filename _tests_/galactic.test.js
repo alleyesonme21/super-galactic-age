141a384d443655d9c1f44c46f0d4f067608f2956
jd <duverseaujohnny21@gmail.com>
2020-09-04 18:05:05 -0400

    first test expectancyMars function  failed

--- _tests_/galactic.test.js
+++ _tests_/galactic.test.js
@@ -36,5 +36,9 @@ describe('Planets', () => {
   test("It will show the users'years left to live on Venus", () => {
     expect(planets.expectancyVenus()).toBe("You have lived -314 past your life expectancy")
   });
+
+  test("It will show the users'years left to live on Mars", () => {
+    expect(planets.expectancyMars()).toBe("")
+  });
   
 })
