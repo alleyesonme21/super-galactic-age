cc96c16f16332a0ab93fad2770da557cfc7f6e45
jd <duverseaujohnny21@gmail.com>
2020-09-18 00:32:34 -0400

    indented main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
- import './css/styles.css';
+import './css/styles.css';
 import Planets from './../src/galactic.js';
 
 // not finished yet
@@ -9,8 +9,8 @@ $(document).ready(function() {
   $('#formOne').submit(function(event) {
     event.preventDefault();
     const age = $('#age').val();
-   const expectancyAge  = $("age-expectancy")
-   const inputRadio = $("input:radio[name=expectancy]:checked").val();
+    const expectancyAge  = $("age-expectancy")
+    const inputRadio = $("input:radio[name=expectancy]:checked").val();
   let planets = new Planets(age, expectancyAge, inputRadio)
   let result = planets.expectancyMercury().age;
   console.log(result);
