3fcef67c9d5a241ac28f07c882e48778f3c9304c
jd <duverseaujohnny21@gmail.com>
2020-10-02 09:03:30 -0400

    indented all line

--- _tests_/galactic.test.js
+++ _tests_/galactic.test.js
@@ -1,56 +1,55 @@
 import Planets from './../src/galactic.js'
 describe('Planets', () => {
 
-  let planets;
+    let planets;
   
-  test("It will show  you the user current age in Earth", () => {
+test("It will show  you the user current age in Earth", () => {
     planets = new Planets(38,90);
-    expect(planets.checkEarth()).toEqual(38)
+    expect(planets.checkEarth()).toEqual(38);
   })
 
-  test("It will show  you the user current age in Mercury", () => {
+test("It will show  you the user current age in Mercury", () => {
     planets = new Planets(40,90);
-    expect(planets.checkMercury()).toEqual(167)
+    expect(planets.checkMercury()).toEqual(167);
   })
 
-  test("It will show you the users current age in Venus", () => {
-  ;planets = new Planets(2,90);
-    expect( planets.checkVenus()).toEqual(3)
+test("It will show you the users current age in Venus", () => {
+    planets = new Planets(2,90);
+    expect( planets.checkVenus()).toEqual(3);
   })
 
-  test("It will show you the users current age in Mars", () => {
+test("It will show you the users current age in Mars", () => {
     planets = new Planets(80,90);
-    expect(planets.checkMars()).toEqual(43)
-  
-  })
-  test("It will show you the users current age in Jupiter", () => {
-    planets = new Planets(100,90);
-    expect(planets.checkJupiter()).toEqual(8)
+    expect(planets.checkMars()).toEqual(43);
   });
-  test("It will show the users'years left to live on Earth", () => {
+
+test("It will show you the users current age in Jupiter", () => {
+    planets = new Planets(100,90);
+    expect(planets.checkJupiter()).toEqual(8);
+  });
+
+test("It will show the users'years left to live on Earth", () => {
     planets = new Planets(120,90);
     expect(planets.expectancyEarth()).toBe("You have lived -30 past your life expectancy")
   });
 
-
 test("It will show the users'years left to live on Mercury", () => {
-      planets = new Planets(75,90);
-      expect(planets.expectancyMercury()).toBe("You have 62 left to live")
+    planets = new Planets(75,90);
+    expect(planets.expectancyMercury()).toBe("You have 62 left to live")
   });
 
-  test("It will show the users'years left to live on Venus", () => {
-      planets = new Planets(25,90);
-      expect(planets.expectancyVenus()).toBe("You have 105 left to live")
+test("It will show the users'years left to live on Venus", () => {
+    planets = new Planets(25,90);
+    expect(planets.expectancyVenus()).toBe("You have 105 left to live")
   });
 
-  test("It will show the users'years left to live on Mars", () => {
+test("It will show the users'years left to live on Mars", () => {
     planets = new Planets(80,90);
     expect(planets.expectancyMars()).toBe("You have 5 left to live")
   });
 
-  test("It will show the users'years left to live on Jupiter", () => {
+test("It will show the users'years left to live on Jupiter", () => {
     planets = new Planets(30,90);
     expect(planets.expectancyJupiter()).toBe("You have 5 left to live")
-  });
-  
+  }); 
 })
